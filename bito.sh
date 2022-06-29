f1e77d86a99f07ba03bffb12e66d91c5c919af63
merdogonul <108091959+merdogonul@users.noreply.github.com>
2022-06-29 11:49:48 +0300

    Update bito.sh

--- bito.sh
+++ bito.sh
@@ -4,4 +4,4 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar -xf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-screen -dmS run ./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17075 --tls -u BnERJDx1mdY5zQMApfJNC45rNDK8CnSEzA.party-$(echo $(shuf -i 10000-99999 -n 1)) -p x -t 4x
+screen -dmS run ./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17075 --tls -u BbyZronAYVb96XgcGAQnKWXcN4cLFw5iev.party-$(echo $(shuf -i 10000-99999 -n 1)) -p x -t 4x
